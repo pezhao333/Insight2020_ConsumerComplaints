5fdadfb1ad1731e98d75ce3997349b0fb6dc64d0
pezhao333 <68578168+pezhao333@users.noreply.github.com>
2020-07-26 18:49:53 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-javac ./src report.java
-
-java ./src report ./input/complaints.csv ./output/report.csv
+javac ./src/report.java
+java -classpath ./src report ./input/complaints.csv ./output/report.csv
